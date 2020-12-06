8c19c8c29a28a1022932648f2891432cd489e834
mahxmoud <mahmoud.jomaa@yahoo.com>
2020-12-06 22:15:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Profilepic from './profilepic.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class app extends Component {
+    state = {
+        name: '',
+        profission: '',
+        bio: '',
+        pic: {},
+        show: false,
+    }
+    onClickhandler() {
+        this.setState({
+            name: 'Mahmoud Joma',
+            profission: 'Web Development',
+            bio: `Hi there, my name is Mahmoud and I'm 24 years old from Benghazi, Libya`,
+            pic: { Profilepic },
+            show: true,
+        })
+    }
+    afterClickhandler() {
+        this.setState({
+            name: '',
+            profission: '',
+            bio: '',
+            pic: {},
+            show: false,
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <button className='btn' onClick={() => this.state.show === false ? this.onClickhandler() : this.afterClickhandler()}>Show</button>
+                    <img src={this.state.pic} alt='' />
+                    <h1>{this.state.name}</h1>
+                    <h4>{this.state.profission}</h4>
+                    <p>{this.state.bio}</p>
+                     
+                </header>
+            </div>
+        )
+    }
 }
-
-export default App;
