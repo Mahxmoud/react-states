887fdd7f87364b965b1d5b9f15ec9ad16b869a65
mahxmoud <mahmoud.jomaa@yahoo.com>
2020-12-07 11:54:51 +0200

    added timer for mounting

--- src/App.js
+++ src/App.js
@@ -9,16 +9,23 @@ export default class app extends Component {
         bio: `Hi there, my name is Mahmoud and I'm 24 years old from Benghazi, Libya`,
         pic: Profilepic,
         show: false,
+        counter: 0,
     }
     onClickhandler() {
         this.setState({
             show: !this.state.show,
         })
     }
+    componentDidMount() {
+        this.setState({ counter: 0 })
+        setInterval(() => this.setState({ counter: this.state.counter + 1 }), 1000)
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
+                    <p >It's been {this.state.counter} seconds since the component got mounted</p>
                     <button className='btn' onClick={() => this.onClickhandler()}>show</button>
                     {this.state.show === true ? <div><img src={this.state.pic} alt='' />
                         <h1>{this.state.name}</h1>
