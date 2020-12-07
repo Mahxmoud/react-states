141efebbba91b592c1d973c39375db33aefb14f7
mahxmoud <mahmoud.jomaa@yahoo.com>
2020-12-07 10:38:09 +0200

    fixed image bug

--- src/App.js
+++ src/App.js
@@ -4,40 +4,26 @@ import Profilepic from './profilepic.jpg'
 
 export default class app extends Component {
     state = {
-        name: '',
-        profission: '',
-        bio: '',
-        pic: {},
+        name: 'Mahmoud Joma',
+        profission: 'Web Development',
+        bio: `Hi there, my name is Mahmoud and I'm 24 years old from Benghazi, Libya`,
+        pic: Profilepic,
         show: false,
     }
     onClickhandler() {
         this.setState({
-            name: 'Mahmoud Joma',
-            profission: 'Web Development',
-            bio: `Hi there, my name is Mahmoud and I'm 24 years old from Benghazi, Libya`,
-            pic: { Profilepic },
-            show: true,
-        })
-    }
-    afterClickhandler() {
-        this.setState({
-            name: '',
-            profission: '',
-            bio: '',
-            pic: {},
-            show: false,
+            show: !this.state.show,
         })
     }
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <button className='btn' onClick={() => this.state.show === false ? this.onClickhandler() : this.afterClickhandler()}>Show</button>
-                    <img src={this.state.pic} alt='' />
-                    <h1>{this.state.name}</h1>
-                    <h4>{this.state.profission}</h4>
-                    <p>{this.state.bio}</p>
-                     
+                    <button className='btn' onClick={() => this.onClickhandler()}>show</button>
+                    {this.state.show === true ? <div><img src={this.state.pic} alt='' />
+                        <h1>{this.state.name}</h1>
+                        <h4>{this.state.profission}</h4>
+                        <p>{this.state.bio}</p></div> : <div></div>}
                 </header>
             </div>
         )
